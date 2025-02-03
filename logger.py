6e7682cc8503fddaf28bee7c346ffe94441e84cc
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2025-02-03 13:28:21 +0900

    不要なファイルを削除

--- logger.py
+++
@@ -1,9 +0,0 @@
-class Logger(object):
-    def info(self, msg):
-        print(f"[INFO] :{msg}")
-        
-    def warn(self, msg):
-        print(f"[WARN] :{msg}")
-        
-    def error(self, msg):
-        print(f"[ERROR] :{msg}")
